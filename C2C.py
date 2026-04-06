f21bf5c5597a81847c63a4fdb5624170febf46a4
B-Cut <kaylagbst@hotmail.com>
2026-04-06 12:57:16 -0300

    Add recon start endpoints

--- C2C.py
+++ C2C.py
@@ -1,10 +1,14 @@
 from flask import Flask, request, Response
 from dotenv import load_dotenv, dotenv_values
 import os
+import subprocess
 
 ROOT_DIR = f"./recon"
 RECON_TARGETS_FILE_PATH = ROOT_DIR + "/" + "targets.txt"
 DOMAINS_DIR_PATH = ROOT_DIR + "/" + "domains"
+
+def check_token(request):
+    return request.form['secret'] != os.getenv('SECRET_KEY')
 
 def init_structure():
     if not os.path.exists(ROOT_DIR):
@@ -18,7 +22,7 @@ app = Flask(__name__)
 @app.route("/add_target/<target_domain>", methods=["POST"])
 def add_target(target_domain):
 
-    if request.form['secret'] != os.getenv('SECRET_KEY'):
+    if not check_token(request):
         return "Authentication Error", 401
 
     with open(RECON_TARGETS_FILE_PATH, "a+") as f:
@@ -33,8 +37,7 @@ def add_target(target_domain):
     
 @app.route("/remove_target/<target_domain>", methods=["POST"])
 def remove_target(target_domain):
-
-    if request.form['secret'] != os.getenv('SECRET_KEY'):
+    if not check_token(request):
         return "Authentication Error", 401
 
     with open(RECON_TARGETS_FILE_PATH, "r+") as f:
@@ -48,6 +51,39 @@ def remove_target(target_domain):
         
     return "OK", 200
 
+@app.route("/run_chain/<target>", methods=["POST"])
+def run_chain_on_target(target):
+    if not check_token(request):
+        return "Authentication Error", 401
+    
+    with open(RECON_TARGETS_FILE_PATH, "r") as f:
+        for line in f.readlines():
+            if target in line:
+                pass
 
+@app.route("/run_full_recon", methods=["POST"])
+def run_full_recon():
+    if not check_token(request):
+        return "Authentication Error", 401
+    
+    if os.path.exists("./recon_lock"):
+        return "Analysis already running", 200
+    
+    subprocess.Popen(["./recon_scripts/run_on_all.sh", "full", os.path.abspath(RECON_TARGETS_FILE_PATH)])
+    
+    return "Initiated full chain analysis on all targets!", 200
 
+@app.route("/run_quick_enumeration_on_all", methods=["POST"])
+def run_quick_recon():
+    if not check_token(request):
+        return "Authentication Error", 401
+    
+    if os.path.exists("./recon_lock"):
+        return "Analysis already running", 200
+
+    subprocess.Popen(["./recon_scripts/run_on_all.sh", "basic", os.path.abspath(RECON_TARGETS_FILE_PATH)])
+    
+    return "Initiated quick analysis on all targets!", 200
+    
+    
 
